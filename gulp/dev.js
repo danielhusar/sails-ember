4d0d1b919b53de4476de0ad874bff295432292d9
Daniel Husar <dano.husar@gmail.com>
2014-06-16 11:16:19 +0100

    public dir

--- gulp/dev.js
+++ gulp/dev.js
@@ -6,11 +6,10 @@ module.exports = function(gulp, jsFiles){
   //watch Files For Changes
   gulp.task('watch', function () {
 
-    require('child_process').spawn('sudo', ['mongod'], {stdio: 'inherit'});
     require('child_process').spawn('sails', ['lift'], {stdio: 'inherit'});
     browser('http://localhost:1337', 'Google Chrome');
 
-    gulp.watch(['less/*.less', 'less/**/*.less', 'less/**/**/*.less', 'less/**/**/**/*.less'], ['less']);
+    gulp.watch(['assets/less/*.less', 'assets/less/**/*.less'], ['less']);
     gulp.watch(jsFiles, ['lint']);
   });
 
